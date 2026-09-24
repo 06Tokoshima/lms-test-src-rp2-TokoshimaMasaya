63ee9d6cd33abadf5de49e323743645b295f5d39
m-tokoshima <m-tokoshima@westnet.co.jp>
2026-09-24 17:23:37 +0900

    add:Case03,Case04 床嶋 雅也 Case03とCase04のSeleniumテストコードを作成

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト ログイン機能①
@@ -18,6 +27,12 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース03 受講生 ログイン 正常系")
 public class Case03 {
+
+	// ポート番号8080番
+	private final int PORT = 8080;
+
+	/** Driver */
+	private WebDriver driver = WebDriverUtils.webDriver;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,14 +50,36 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:" + PORT + "/lms");
+		assertEquals("ログイン | LMS", driver.getTitle());
+		assertEquals("ログイン", driver.findElement(By.tagName("h2")).getText());
+		assertEquals("ログイン", driver.findElement(By.className("btn-primary")).getAttribute("value"));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
-	}
+		driver.findElement(By.name("loginId")).clear();
+		driver.findElement(By.name("loginId")).sendKeys("StudentAA03");
 
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("Sakura1120");
+
+		driver.findElement(By.className("btn-primary")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("h2")));
+
+		assertEquals("コース詳細 | LMS", driver.getTitle());
+
+		String h2Text = driver.findElement(By.tagName("h2")).getText();
+		assertTrue(h2Text.contains("DEMOコース"));
+		assertTrue(h2Text.contains("2025年7月1日"));
+
+		getEvidence(new Object() {
+		});
+	}
 }
--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import jp.co.sss.lms.ct.util.WebDriverUtils;
 
 /**
  * 結合テスト よくある質問機能
@@ -18,6 +27,12 @@ import org.junit.jupiter.api.TestMethodOrder;
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース04 よくある質問画面への遷移")
 public class Case04 {
+
+	// ポート番号8080番
+	private final int PORT = 8080;
+
+	/** Driver */
+	private WebDriver driver = WebDriverUtils.webDriver;
 
 	/** 前処理 */
 	@BeforeAll
@@ -35,28 +50,79 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:" + PORT + "/lms");
+		assertEquals("ログイン | LMS", driver.getTitle());
+		assertEquals("ログイン", driver.findElement(By.tagName("h2")).getText());
+		assertEquals("ログイン", driver.findElement(By.className("btn-primary")).getAttribute("value"));
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		driver.findElement(By.name("loginId")).clear();
+		driver.findElement(By.name("loginId")).sendKeys("StudentAA03");
+
+		driver.findElement(By.name("password")).clear();
+		driver.findElement(By.name("password")).sendKeys("Sakura1120");
+
+		driver.findElement(By.className("btn-primary")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs("コース詳細 | LMS"));
+
+		assertEquals("コース詳細 | LMS", driver.getTitle());
+
+		String h2Text = driver.findElement(By.tagName("h2")).getText();
+		assertTrue(h2Text.contains("DEMOコース"));
+
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		driver.findElement(By.className("dropdown-toggle")).click();
+		driver.findElement(By.linkText("ヘルプ")).click();
+
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleIs("ヘルプ | LMS"));
+
+		assertEquals("ヘルプ | LMS", driver.getTitle());
+		assertEquals("ヘルプ", driver.findElement(By.tagName("h2")).getText());
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
-	}
+		String Handle = driver.getWindowHandle();
+		driver.findElement(By.linkText("よくある質問")).click();
 
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+
+		String newHandle = null;
+		for (String id : driver.getWindowHandles()) {
+			if (!id.equals(Handle)) {
+				newHandle = id;
+			}
+		}
+
+		driver.switchTo().window(newHandle);
+
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.tagName("h2")));
+
+		assertEquals("よくある質問 | LMS", driver.getTitle());
+
+		assertEquals("よくある質問", driver.findElement(By.tagName("h2")).getText());
+		getEvidence(new Object() {
+		});
+	}
 }
